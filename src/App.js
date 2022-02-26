3af2f7aab30a0b75571729f6236e78f0a8e3868c
Zahra Rabiei <85116470+Zahra947@users.noreply.github.com>
2022-02-26 14:04:10 +0330

    made header component and added bs dependancy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./styles.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
+import Header from "./Header";
 export default function App() {
   return (
     <Router>
