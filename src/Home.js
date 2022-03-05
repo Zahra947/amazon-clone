5164a9b3c5efb7832debaee6a1b97947b43a881d
Zahra Rabiei <85116470+Zahra947@users.noreply.github.com>
2022-03-05 14:20:42 +0330

    added home.css file

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,14 @@
 import React from "react";
+import "./styles/Home.css";
 
 export default function Home() {
-  return <h1>hiiii</h1>;
+  return (
+    <div className="home">
+      <img
+        className="home__image"
+        src="https://wallpaperaccess.com/full/2731342.jpg"
+        alt="amazon-prime"
+      />
+    </div>
+  );
 }
