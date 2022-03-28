d24231ba799996289158ea2516188f3db89f5761
Zahra Rabiei <85116470+Zahra947@users.noreply.github.com>
2022-03-28 16:21:28 +0430

    made checkout component

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { useStateValue } from "./StateProvider";
+import "./styles/checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
 
 export default function Checkout() {
-  return <h1> check out</h1>;
+  const [{ basket }] = useStateValue();
+
+  return (
+    <div className="checkout">
+      {basket?.length === 0 ? (
+        <div>
+          <h1>Your basket is empty</h1>
+          <p>
+            You have no item in your basket. To buy one, press "Add to basket"
+            next to the item.{" "}
+          </p>
+        </div>
+      ) : (
+        <h1 className="checkout__title">
+          Your basket is:
+          {basket.map((item) => (
+            <CheckoutProduct />
+          ))}
+        </h1>
+      )}
+    </div>
+  );
 }
---
+++ src/CheckoutProduct.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function CheckoutProduct(props) {
+  return <div></div>;
+}
