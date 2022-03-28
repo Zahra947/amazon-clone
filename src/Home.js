efd84669e8653945593a973f257ce3a6d9f12c80
Zahra Rabiei <85116470+Zahra947@users.noreply.github.com>
2022-03-28 10:55:59 +0430

    created product component added props to the component

--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
         src="https://wallpaperaccess.com/full/2731342.jpg"
         alt="amazon-prime"
       />
-      <Product />
+      <Product id={123456} title="new book" price={2} rating={5} />
     </div>
   );
 }
--- src/Product.js
+++ src/Product.js
@@ -2,5 +2,20 @@ import React from "react";
 import "./styles/Product.css";
 
 export default function Product(props) {
-  return <div className="product">hi</div>;
+  return (
+    <div className="product">
+      <p>{props.title}</p>
+      <p className="product__price">
+        <small>$</small>
+        <strong>{props.price}</strong>
+      </p>
+      <div className="product__rating">
+        {Array(props.rating)
+          .fill()
+          .map((_) => (
+            <span>⭐</span>
+          ))}
+      </div>
+    </div>
+  );
 }
