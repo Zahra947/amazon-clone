d89b2b09bde963baad21b5bb8f5b36b32cffbd53
Zahra Rabiei <85116470+Zahra947@users.noreply.github.com>
2022-03-28 13:50:08 +0430

    complete product component design with css

--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,54 @@ export default function Home() {
         src="https://wallpaperaccess.com/full/2731342.jpg"
         alt="amazon-prime"
       />
-      <Product id={123456} title="new book" price={2} rating={5} />
+      <div className="home__row">
+        <Product
+          id={123456}
+          title="new book"
+          price={2}
+          rating={5}
+          image="https://www.publicbooks.org/wp-content/uploads/2017/01/book-e1484158615982-810x327.jpg"
+        />
+        <Product
+          id={123456}
+          title="new book"
+          price={2}
+          rating={5}
+          image="https://www.publicbooks.org/wp-content/uploads/2017/01/book-e1484158615982-810x327.jpg"
+        />
+      </div>
+      <div className="home__row">
+        <Product
+          id={123456}
+          title="new book"
+          price={2}
+          rating={5}
+          image="https://www.publicbooks.org/wp-content/uploads/2017/01/book-e1484158615982-810x327.jpg"
+        />
+        <Product
+          id={123456}
+          title="new book"
+          price={2}
+          rating={5}
+          image="https://www.publicbooks.org/wp-content/uploads/2017/01/book-e1484158615982-810x327.jpg"
+        />
+        <Product
+          id={123456}
+          title="new book"
+          price={2}
+          rating={5}
+          image="https://www.publicbooks.org/wp-content/uploads/2017/01/book-e1484158615982-810x327.jpg"
+        />
+      </div>
+      <div className="home__row">
+        <Product
+          id={123456}
+          title="new book"
+          price={2}
+          rating={5}
+          image="https://www.publicbooks.org/wp-content/uploads/2017/01/book-e1484158615982-810x327.jpg"
+        />
+      </div>
     </div>
   );
 }
--- src/Product.js
+++ src/Product.js
@@ -4,18 +4,24 @@ import "./styles/Product.css";
 export default function Product(props) {
   return (
     <div className="product">
-      <p>{props.title}</p>
-      <p className="product__price">
-        <small>$</small>
-        <strong>{props.price}</strong>
-      </p>
-      <div className="product__rating">
-        {Array(props.rating)
-          .fill()
-          .map((_) => (
-            <span>⭐</span>
-          ))}
+      <div className="product__info">
+        <p>{props.title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{props.price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(props.rating)
+            .fill()
+            .map((_) => (
+              <span role="img" aria-label="star">
+                ⭐
+              </span>
+            ))}
+        </div>
       </div>
+      <img src={props.image} alt="a book" />
+      <button>Add to basket</button>
     </div>
   );
 }
