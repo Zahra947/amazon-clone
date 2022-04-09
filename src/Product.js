d3f8d7b1b39d698222fa73ccf06fa5104c61f06b
Zahra Rabiei <85116470+Zahra947@users.noreply.github.com>
2022-04-09 13:12:52 +0430

    add basket error is appeared that dispatch is not a function

--- src/Product.js
+++ src/Product.js
@@ -3,7 +3,7 @@ import "./styles/Product.css";
 import { useStateValue } from "./StateProvider";
 
 export default function Product(props) {
-  const [dispatch] = useStateValue();
+  const [{ basket }, dispatch] = useStateValue();
 
   const addToBasket = () => {
     //add to basket
