1a81fb1d64d445535a33c6040bfcd3d1a3808055
Zahra Rabiei <85116470+Zahra947@users.noreply.github.com>
2022-04-15 20:36:43 +0430

    added the function to reducer getBasketTotal is the function

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,9 +1,31 @@
 import React from "react";
 import "./styles/subtotal.css";
+import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "./StateProvider";
+import { getBasketTotal } from "./reducer";
 
 export default function Subtotal(props) {
+  const [{ basket }] = useStateValue();
   return (
     <div className="subtotal">
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal({basket.lenght} items): <strong>"$"</strong>
+            </p>
+            <small className="subtotal__goft">
+              <input type="checkbox" />
+              This order contains a gift.
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={getBasketTotal(basket)}
+        displayType={"text"}
+        thousandSeperator={true}
+        prefix={"$"}
+      />
       <button>Proceed to checkout</button>
     </div>
   );
