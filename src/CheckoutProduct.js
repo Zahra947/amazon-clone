58803cb94ae15b917d4c3ee6eca7fa132f77cc5e
Zahra Rabiei <85116470+Zahra947@users.noreply.github.com>
2022-04-15 18:48:43 +0430

    made checkout page and design it

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,5 +1,32 @@
 import React from "react";
+import "./styles/checkout.css";
 
 export function CheckoutProduct(props) {
-  return <div></div>;
+  function removeFromBasket() {}
+  return (
+    <div className="checkoutProduct">
+      <img
+        className="checkoutProduct__image"
+        src={props.image}
+        alt={props.title}
+      />
+      <div className="checkoutProduct__info">
+        <p className="checkoutProduct__title">{props.title}</p>
+        <p className="checkoutProduct__price">
+          <small>$</small>
+          <strong>{props.price}</strong>
+        </p>
+        <div className="checkoutProduct__rating">
+          {Array(props.rating)
+            .fill()
+            .map((_) => (
+              <span role="img" aria-label="star">
+                ⭐
+              </span>
+            ))}
+        </div>
+        <button onClick={removeFromBasket}>Remove from basket</button>
+      </div>
+    </div>
+  );
 }
