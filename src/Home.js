16061dc726b3e952dd193bce1c2f43a2831a66c5
Zahra Rabiei <85116470+Zahra947@users.noreply.github.com>
2022-04-23 18:03:25 +0430

    changed ids of products

--- src/Home.js
+++ src/Home.js
@@ -12,14 +12,14 @@ export default function Home() {
       />
       <div className="home__row">
         <Product
-          id={123456}
+          id={123}
           title="Guess Perfume - Guess 1981 - perfumes for women, 100 ml - EDT Spray"
           price={50}
           rating={5}
           image="https://m.media-amazon.com/images/I/61Xv4lrqk8L._AC_SX425_.jpg"
         />
         <Product
-          id={123457}
+          id={457}
           title="BOSS The Scent Private Accord for Him, 1.6 Fl Oz"
           price={60}
           rating={5}
@@ -28,21 +28,21 @@ export default function Home() {
       </div>
       <div className="home__row">
         <Product
-          id={123458}
+          id={8910}
           title="Neutrogena Hydro Face Moisturizer, 1.7 oz"
           price={17}
           rating={4}
           image="https://m.media-amazon.com/images/I/71ICfUvvEyL._SY355_.jpg"
         />
         <Product
-          id={123459}
+          id={111213}
           title="Apple AirPods (2nd Generation)"
           price={600}
           rating={5}
           image="https://m.media-amazon.com/images/I/7120GgUKj3L._AC_SX385_.jpg"
         />
         <Product
-          id={123466}
+          id={141516}
           title="Hair Dryer and Hot Air Brush, Black"
           price={100}
           rating={5}
@@ -51,7 +51,7 @@ export default function Home() {
       </div>
       <div className="home__row">
         <Product
-          id={123476}
+          id={171819}
           title="SAMSUNG 32-inch Class LED Smart FHD TV 1080P (UN32N5300AFXZA, 2018 Model)"
           price={1500}
           rating={5}
